4a35542213a156d7e3454be4bcff3a72d148a181
Beck <169068547+becklindo@users.noreply.github.com>
2024-09-03 01:02:35 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    System.out.println("olá qual seu nome?");
+    String nome = sc.nextLine();
+    System.out.println("digite sua idade");
+    int idade = sc.nextInt();
+    System.out.println("digite sua altura");
+    double altura = sc.nextDouble();
+    System.out.println("digite 1 para feminino e 2 para masculino");
+    
+    
+  if (sc.nextInt() ==1);
+    
+    System.out.println("seu peso ideal é");
+    System.out.printf("%.2f", (72.7 * altura) - 58);
+
+    if (sc.nextInt() ==2);
+    System.out.println("seu peso ideal é");
+    System.out.printf("%.2F, (62.7 * altura) - 44.7");
+    System.out.println("obrigado por usar nosso programa!");
+
+    
+
+  }
